a3496ca5c5507f678e03c74d163f1e8697d19527
estrattonbailey <ericstrattonbailey@gmail.com>
2019-03-19 12:04:53 -0400

    update styled-components contract

--- packages/plugin-styled-components/rola-plugin-styled-components.js
+++ packages/plugin-styled-components/rola-plugin-styled-components.js
@@ -1,29 +1,33 @@
 /** @jsx React.createElement*/
 import React from 'react'
-import { ServerStyleSheet } from 'styled-components'
+import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
 
 const sheets = new Map()
 
 export default (options = {}) => {
   return {
     createConfig ({ config, context }) {
-      confing.plugins.push('babel-plugin-styled-components')
+      config.plugins.push([
+        'babel-plugin-styled-components',
+        { ssr: true }
+      ])
     },
     wrapApp ({ component, context }) {
       const sheet = new ServerStyleSheet()
 
       sheets.set(context.pathname, sheet)
 
-      return sheet.collectStyles(component)
+      return <StyleSheetManager sheet={sheet.instance}>{component}</StyleSheetManager>
     },
     appDidRender ({ context }) {
       const sheet = sheets.get(context.pathname)
 
       if (!sheet) return {}
 
-      const style = sheet.getStyleTags()
+      const style = sheet.getStyleElement()
 
       sheet.seal()
+      sheets.delete(context.pathname)
 
       return { style }
     }
